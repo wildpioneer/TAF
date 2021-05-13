4ca84fb4c58dd5e57f0240d66747562306d1b4cd
Alexandr Trostyanko <Aleksandr.Trostyanko@itechart-group.com>
2021-05-13 20:05:39 +0300

    Initial framework creation

---
+++ src/main/java/core/ReadProperties.java
@@ -0,0 +1,28 @@
+package core;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class ReadProperties {
+    private static Properties properties = null;
+    private static String filename = "config.properties";
+
+    static {
+        properties = new Properties();
+        try {
+            properties.load(ReadProperties.class.getClassLoader().getResourceAsStream(filename));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getUrl() {
+        return properties.getProperty("url");
+    }
+    public static String getUsername() {
+        return properties.getProperty("username");
+    }
+    public static String getPassword() {
+        return properties.getProperty("password");
+    }
+}
