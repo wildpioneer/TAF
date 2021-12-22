8c1e80eac00c67561e1160115e53e82bb0619bdc
Alexandr Trostyanko <wildpioneer@yandex.ru>
2021-12-22 15:59:29 +0300

    Fixed

--- src/main/java/Listener.java
+++ src/main/java/Listener.java
@@ -3,7 +3,9 @@ import org.testng.ITestResult;
 
 public class Listener implements ITestListener {
     public void onTestFailure(ITestResult result) {
+        System.out.println("=========================================");
         System.out.println("Make screenshot.");
         System.out.println("Status is: " + result.getStatus());
+        System.out.println("=========================================");
     }
 }
--- src/test/java/PriorityTest.java
+++ src/test/java/PriorityTest.java
@@ -2,7 +2,7 @@ import org.testng.annotations.Test;
 
 public class PriorityTest {
 
-    @Test (priority = 1)
+    @Test (priority = 1, description = "first test")
     public void stepb() {
         System.out.println("Step 1...");
     }
@@ -12,12 +12,17 @@ public class PriorityTest {
         System.out.println("Step 2...");
     }
 
-    @Test (priority = 2)
-    public void stepa() {
+    @Test (priority = 3)
+    public void stepa11() {
         System.out.println("Step 3...");
     }
 
-    @Test (priority = 2)
+    @Test (priority = 3)
+    public void stepa1() {
+        System.out.println("Step 3...");
+    }
+
+    @Test (priority = 4)
     public void stepc() {
         System.out.println("Step 4...");
     }
